6c930fbf0dc0c74f9961fe7d95a7e2ff82aeb594
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-10-01 18:25:05 -0400

    outline of code

---
+++ reader.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+/*
+ * Reader Program
+ * Lab 5
+ * 
+ * @author Kasey Stowell + Kehlsey Lewis
+ * @version October 2018
+ *
+ */
+
+int main() {
+    // read new string from shared memory
+    // display string on screen
+}
