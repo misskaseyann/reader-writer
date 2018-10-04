50e78c4d9982616b7f7e52155a27bd3e6650e9cc
Kehlsoo <lewiskeh@mail.gvsu.edu>
2018-10-03 21:25:42 -0400

    started

--- reader.c
+++ reader.c
@@ -17,4 +17,47 @@
 int main() {
     // read new string from shared memory
     // display string on screen
+
+	key_t key;
+	int shmId;
+	char *shmPtr, *message;
+
+
+    //generate key 
+	key = ftok("here",'k'); 
+
+	printf("here\n");
+
+    //returns an identifier in shmid 
+	if ((shmId = shmget(key,4096,S_IRUSR|S_IWUSR)) < 0){
+		perror("shared memory error");
+		exit(1);
+	} 
+
+	if((shmPtr = shmat(shmId, 0, 0)) == (void*) -1) {
+		perror("Can't attach\n");
+		exit(1);
+	}
+
+
+	printf("reader ready to go!\n");
+    // shmat to attach to shared memory  
+
+	//flag for knowing if string was read
+	//flag = (char*) shmat(shmId,(void*)0,0);
+
+
+	if (shmdt (shmPtr) < 0) {
+		perror ("just can't let go\n");
+		exit (1);
+	}
+
+	if (shmctl (shmId, IPC_RMID, 0) < 0) {
+		perror ("can't deallocate\n");
+		exit(1);
+	}
+
+	printf("reader read: %s\n", message);
+
+	return 0; 
 }
--- writer.c
+++ writer.c
@@ -13,9 +13,49 @@
  * @version October 2018
  *
  */
+#define MAX
 
 int main() {
     // creates shared memory segment
     // repeatedly accepts an arbitrary user input string from terminal
     // writes string into shared memory
+
+	int shmId;
+	char *shmPtr;
+	key_t key;
+	char *message[MAX];
+
+    //generate a key 
+	key = ftok("here",'k'); 
+
+	if((shmId = shmget(key, 4096, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0 ) {
+		perror("Error creating shared memory\n");
+		exit(1);
+	} 
+
+	if((shmPtr = shmat(shmId, 0, 0)) == (void*) -1) {
+		perror("Can't attach\n");
+		exit(1);
+	}
+
+	//flag for knowing if string was read
+	//flag = (char*) shmat(shmId,(void*)0,0);
+
+
+	printf("Enter a message: \n" ); 
+	scanf("%s", message);
+
+
+    //detach   
+	if (shmdt (shmPtr) < 0) {
+		perror ("just can't let go\n");
+		exit (1);
+	}
+
+	if (shmctl (shmId, IPC_RMID, 0) < 0) {
+		perror ("can't deallocate\n");
+		exit(1);
+	}
+
+	return 0; 
 }
